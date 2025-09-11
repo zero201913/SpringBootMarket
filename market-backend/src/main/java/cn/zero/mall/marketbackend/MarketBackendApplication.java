fd058a1f79d5b33f1167172e2f5cbbc3167c4d0e
weixin_52555277 <weixin_52555277@gitcode.net>
2025-09-11 21:05:00 +0800

    first commit

---
+++ market-backend/src/main/java/cn/zero/mall/marketbackend/MarketBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.zero.mall.marketbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketBackendApplication.class, args);
+    }
+
+}
